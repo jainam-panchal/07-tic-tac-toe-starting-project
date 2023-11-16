c1b5cec7ee1119c4bb6de2165e229f2e85fdcd3d
Jainam Panchal <jainampanchal1@gmail.com>
2023-11-17 01:03:33 +0530

    Refactor active player derivation in handleSelectSquare function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Gameboard from "./components/GameBoard.jsx";
 import { useState } from "react"
 import Log from "./components/Log.jsx";
 
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X'
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O'
+    }
+    return currentPlayer
+}
+
 function App() {
     const [gameTurns, setGameTurns] = useState([])
 
@@ -10,18 +18,21 @@ function App() {
     // Lift the state up to the closest ancestor component that has access to all components that need to work with that state
     // here we need activePlayer in both Player component as well as in Gameboard
 
-    const [activePlayer, setActivePlayer] = useState('X');
+    // not using it because we can derive it with turns
+    //const [activePlayer, setActivePlayer] = useState('X');
+    let activePlayer = deriveActivePlayer(gameTurns)
 
     function handleSelectSquare(rowIndex, colIndex) {
         //change game turn
         setActivePlayer(wasActivePlayer => wasActivePlayer === 'X' ? 'O' : 'X')
         setGameTurns((prevTurns) => {
 
-            // at the current point
-            let currentPlayer = 'X'
-            if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
-                currentPlayer = 'O'
-            }
+            // NOTE: at the current point
+            //let currentPlayer = 'X'
+            //if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+            //    currentPlayer = 'O'
+            //}
+            const currentPlayer = deriveActivePlayer(prevTurns)
 
             const updatedTurns = [
                 // NOT A GOOD IDEA BECAUSE activePlayer is dependent
